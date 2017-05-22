d739879c315121e2efe1eefc4e3e6a13be7b0791
Mitchell Smoot <mitchsmoot@Mitchells-MacBook-Pro.local>
2017-05-22 09:53:07 -0600

    Dev tool initial commit

---
+++ lib/smooticus_view_tool.rb
@@ -0,0 +1,6 @@
+require "smooticus_view_tool/version"
+require "smooticus_view_tool/renderer"
+
+module SmooticusViewTool
+  # Your code goes here...
+end
---
+++ lib/smooticus_view_tool/version.rb
@@ -0,0 +1,3 @@
+module SmooticusViewTool
+  VERSION = "0.1.0"
+end
